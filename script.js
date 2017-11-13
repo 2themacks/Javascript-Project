1b92b77a54bbd093008dbb79e6787835f03cf080
2themacks <32740624+2themacks@users.noreply.github.com>
2017-11-13 15:47:41 -0600

    Add files via upload
    Fixed error messages and added game directions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let changeAnswerObjCase;
+let answerButton = document.getElementById('answerButton');
 
 function randomQ () {
   var ajaxRequest = new XMLHttpRequest ()
@@ -24,6 +25,14 @@ function randomQ () {
           answer.innerHTML = changeAnswerObjCase;
 
           showAnswerButton();
+//this code clears the answer fields whent the question button is clicked.
+          document.getElementById("userAnswer").innerHTML = "";
+          document.getElementById("typedAnswer").innerHTML = "";
+          document.getElementById("theanswer").innerHTML = "";
+          let hideAnswerAgain = document.getElementById("answer");
+          hideAnswerAgain.className = "hide";
+          itsRightBtn.className="hide";
+          document.getElementById("answerMessage").innerHTML = "";
       }
       }
     }
@@ -33,7 +42,6 @@ function randomQ () {
 }
 // THE FOLLOWING FUNCTION SHOWS THE ANSWER BUTTON ONCE THE GAME IS POPULATED
 function showAnswerButton() {
-  let answerButton = document.getElementById('answerButton');
   answerButton.className = "";
 }
 
@@ -55,16 +63,20 @@ function showAndCompareAnswer() {
     keepScore();
   }
   else {
-    //creates button
-    let itsRightBtn = document.createElement("button");
-    itsRightBtn.innerHTML = "I got it right";
-    let itsRightBtnDiv = document.getElementById("itsRightBtnDiv");
-    itsRightBtnDiv.appendChild(itsRightBtn);
-    //adds to score if user got it right but there was a typo
-    itsRightBtn.addEventListener("click", keepScore);
-    resetGame();
+    keepScoreFailure();
+    itsRightBtn.className="";
   }
+  answerButton.className = "hide";
+  document.getElementById("userAnswer").value= "";
 }
+
+
+//adds to score if user got it right but there was a typo
+let itsRightBtn = document.getElementById("itsRightBtn");
+itsRightBtn.addEventListener("click", keepScore);
+
+
+
 // or let start score = to session storage once i get that coded
 let startScore=parseInt(0);
 
@@ -76,30 +88,20 @@ function keepScore() {
   if (true){
     startScore = startScore + questionValueNumber;
     document.getElementById("score").innerHTML=startScore;
-    let resetBtn = document.getElementById("resetBtn")
-    resetBtn.className = "";
-    // BELOW CODE IS FOR SESSION STORAGE TBD
-        // sessionStorage.setItem('countingScore', 'questionValueNumber');
-        // let updateScore = sessionStorage.getItem("score");
-        // saveScore()
+    itsRightBtn.className="hide";
+    document.getElementById("answerMessage").innerHTML = "yay you got it right! You're so smart. Click the question button again for another question.";
+  }
+}
+function keepScoreFailure() {
+  let scoreBox = document.getElementById("score");
+  let questionValue = document.getElementById("value").innerHTML;
+  let questionValueNumber = parseInt(questionValue);
+  document.getElementById("answerMessage").innerHTML = "Aw man, that's not the answer we were looking for. Did you get it right and there was a typo? Click the 'I got it Right' button too get the points.";
+  document.getElementById("score").innerHTML=startScore
+  if (true){
+    document.getElementById("score").innerHTML=startScore;
   }
 }
 
-
 document.getElementById("clickMe").addEventListener('click', randomQ);
 document.getElementById('answerButton').addEventListener('click', showAndCompareAnswer);
-document.getElementById('resetBtn').addEventListener('click', resetGame)
-
-
-//THIS IS THE FUNCTION I WILL WRITE TO CREATE ANOTHER ROUND
-function resetGame (){
-  document.getElementById("category").innerHTML = "";
-  document.getElementById("question").innerHTML = "";
-  document.getElementById("value").innerHTML = "";
-  document.getElementById("userAnswer").innerHTML = "";
-  document.getElementById("typedAnswer").innerHTML = "";
-  document.getElementById("theanswer").innerHTML = "";
-  let hideAnswerAgain = document.getElementById("answer");
-  hideAnswerAgain.className = "hide";
-  randomQ();
-}
