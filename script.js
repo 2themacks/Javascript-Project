a5bfbf7de842fcdb5b5b364b77e48e1d872359c8
2themacks <32740624+2themacks@users.noreply.github.com>
2017-11-13 08:01:45 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let changeAnswerObjCase;
+
 function randomQ () {
   var ajaxRequest = new XMLHttpRequest ()
   ajaxRequest.onreadystatechange = function () {
@@ -7,22 +9,21 @@ function randomQ () {
         for (let i=0; i<jsonObj.length; i++) {
           let category = document.getElementById("category");
           category.innerHTML = jsonObj[i].category.title;
-
           let question = document.getElementById("question");
           question.innerHTML = jsonObj[i].question;
-
           let value = document.getElementById("value");
           value.innerHTML = jsonObj[i].value;
-//take out these next two lines of code once you know that the same array info is being pulled.
-          let theanswer = document.getElementById("theanswer");
-          theanswer.innerHTML = jsonObj[i].answer;
+          //delcared this variable valueAction so that i can add it to score board later
+          let valueAction = jsonObj[i].value;
 
-          function showAnswer() {
-            let answer = document.getElementById("answer");
-            answer.innerHTML = jsonObj[i].answers
-          }
+          let answer = document.getElementById("answer");
+          let answerValue = jsonObj[i].answer;
+          console.log(answerValue);
+          //variable changeAnswerObjCase is so i can compare strings
+          changeAnswerObjCase = answerValue.toLowerCase();
+          answer.innerHTML = changeAnswerObjCase;
+
           showAnswerButton();
-          showAnswer();
       }
       }
     }
@@ -30,41 +31,75 @@ function randomQ () {
   ajaxRequest.open("GET","http://jservice.io/api/random", true);
   ajaxRequest.send();
 }
-
-
+// THE FOLLOWING FUNCTION SHOWS THE ANSWER BUTTON ONCE THE GAME IS POPULATED
 function showAnswerButton() {
   let answerButton = document.getElementById('answerButton');
   answerButton.className = "";
 }
 
-//use the this function to show answer
-//how do i pull a function inside another function
-function unhideAnswer() {
-  let unhideAnswer = document.getElementById('answer');
-  answer.className = "answerText";
-  unhideAnswer.innerHTML = showAnswer();
 
+//THIS FUNCTION SHOWS THE USER INPUT ANSWER, THE ACTUAL ANSWER, AND COMPARES THE TWO
+function showAndCompareAnswer() {
+  //this unhides the answer from the JSON object
+  let unhideAnswer = document.getElementById("answer");
+  unhideAnswer.className = "showingAnswer";
+  //this takes the user's input value and converts it to all lowercase
+  let userAnswer = document.getElementById('userAnswer').value;
+  let changeCase = userAnswer.toLowerCase();
+  let typedAnswer = document.getElementById('typedAnswer');
+  typedAnswer.innerHTML = changeCase;
+  console.log(changeCase);
+  console.log(changeAnswerObjCase);
+  if (changeAnswerObjCase == changeCase) {
+    console.log('match');
+    keepScore();
+  }
+  else {
+    //creates button
+    let itsRightBtn = document.createElement("button");
+    itsRightBtn.innerHTML = "I got it right";
+    let itsRightBtnDiv = document.getElementById("itsRightBtnDiv");
+    itsRightBtnDiv.appendChild(itsRightBtn);
+    //adds to score if user got it right but there was a typo
+    itsRightBtn.addEventListener("click", keepScore);
+    resetGame();
+  }
+}
+// or let start score = to session storage once i get that coded
+let startScore=parseInt(0);
+
+function keepScore() {
+  let scoreBox = document.getElementById("score");
+  let questionValue = document.getElementById("value").innerHTML;
+  let questionValueNumber = parseInt(questionValue);
+  document.getElementById("score").innerHTML=startScore
+  if (true){
+    startScore = startScore + questionValueNumber;
+    document.getElementById("score").innerHTML=startScore;
+    let resetBtn = document.getElementById("resetBtn")
+    resetBtn.className = "";
+    // BELOW CODE IS FOR SESSION STORAGE TBD
+        // sessionStorage.setItem('countingScore', 'questionValueNumber');
+        // let updateScore = sessionStorage.getItem("score");
+        // saveScore()
+  }
 }
 
+
 document.getElementById("clickMe").addEventListener('click', randomQ);
-document.getElementById('answerButton').addEventListener('click', unhideAnswer);
+document.getElementById('answerButton').addEventListener('click', showAndCompareAnswer);
+document.getElementById('resetBtn').addEventListener('click', resetGame)
 
 
-
-
-
-//have two buttons: category picker and then a randomizer
-//pick a jeopardy season, then date, then category
-//also have a randomizer
-
-
-//set up another event that shows the answer from the same array at the click of a button\
-// when the button is clicked, it shows two more objects from that array
-// if (randomQ function is executed successfully) {
-    // return button that will take information from that same random array
-// }
-
-
-
-
-// I would like you to try to check the user's answer and keep score automatically. This will be a little hard to do because spelling will be important, so maybe give the user a button to override the system if they knew the answer but misspelled it. Does that make sense? One quick tip on this one: it might be a good idea to convert the answer key to all lowercase and also convert the user's answer to all lowercase when you compare them so you can eliminate the need for the user's input to be case sensitive.
+//THIS IS THE FUNCTION I WILL WRITE TO CREATE ANOTHER ROUND
+function resetGame (){
+  document.getElementById("category").innerHTML = "";
+  document.getElementById("question").innerHTML = "";
+  document.getElementById("value").innerHTML = "";
+  document.getElementById("userAnswer").innerHTML = "";
+  document.getElementById("typedAnswer").innerHTML = "";
+  document.getElementById("theanswer").innerHTML = "";
+  let hideAnswerAgain = document.getElementById("answer");
+  hideAnswerAgain.className = "hide";
+  randomQ();
+}
